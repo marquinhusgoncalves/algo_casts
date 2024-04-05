697bb69b70e16ed91c28b4696978b168612965ec
Marquinhus Gonçalves <eu@marquinhusgoncalves.com>
2024-04-05 12:27:47 +0100

    String Reversal - Solution 2

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,8 +7,17 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 // Solution 1
+// function reverse(str) {
+//   return str.split("").reverse().join("");
+// }
+
+// Solutions 2
 function reverse(str) {
-  return str.split("").reverse().join("");
+  let reversed = "";
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+  return reversed;
 }
 
 module.exports = reverse;
