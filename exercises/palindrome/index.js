b37f46537851b54e9d329b4372539376015a7f5d
Marquinhus Gonçalves <eu@marquinhusgoncalves.com>
2024-04-06 14:42:43 +0100

    Palindromes - Solution 2

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,12 +7,18 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {
-  const reversed = str
-    .split("")
-    .reduce((reversed, character) => character + reversed);
+// function palindrome(str) {
+//   const reversed = str
+//     .split("")
+//     .reduce((reversed, character) => character + reversed);
 
-  return str === reversed;
+//   return str === reversed;
+// }
+
+function palindrome(str) {
+  return str.split("").every((char, i) => {
+    return char === str[str.length - i - 1];
+  });
 }
 
 module.exports = palindrome;
